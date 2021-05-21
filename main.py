e81ea677529edafdd81248b23182e9f59b8776a6
CmdCoder585 <47717085+CmdCoder585@users.noreply.github.com>
2021-05-21 15:51:26 +0100

    Modified commit with intergrated functionalily

--- main.py
+++ main.py
@@ -17,13 +17,18 @@ def kill_test(systray):
     print('Terminating test')
     sendSignal(50000)
 
+def kill_server(systray):
+    print('Terminating Server_V2')
+    sendSignal(54320)
+
 
 def on_quit_callback(systray):
     systray.shutdown()
 
 
 def main():
-    menu_options = (("Kill Test", None, kill_test),)
+    menu_options = (("Kill Test", None, kill_test),
+                    ('Kill Server_V2', None, kill_server))
     systray = SysTrayIcon("Copyright.ico", "Example tray icon", menu_options, on_quit=on_quit_callback)
     systray.start()
 
